a98b1a2c43b69cfc4b7e5fefe47ebea266fd2859
monnand <monnand@a0b449d3-5045-0410-a152-1d95290f8afa>
2008-02-20 17:01:27 +0000

    2008-2-19 monnand<monnand@gmail.com> * gtk-tagal     Finally, I found xdg-open... -_-||     I have just accessed freedesktop.org. Seems a lot of things can be easily done.

--- src/temp.sql
+++
@@ -1,6 +0,0 @@
-select distinct f.id, f.name, f.path from files as f, tag_file_map as map where f.id = map.fid 	and (select count(tid) from tag_file_map, tags where tid = tags.id and fid = map.fid and (tag = 'tag2' or tag = 'tag3')) = 2;
-
-insert or ignore into tag_file_map (fid, tid) select f.id, t.id from files as f, tags as t where f.id = 3 and t.id = 4;
-update tags set nr_ref=nr_ref-1 where tag='new one';
-
-update tags set nr_ref=nr_ref-1 where id in (select t.id from tags as t, files as f, tag_file_map as m where f.id=m.fid and f.path='/home/32' and t.id=m.tid);
